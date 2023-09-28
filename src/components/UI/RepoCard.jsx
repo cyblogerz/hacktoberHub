ac391374e1981659a47d1afd9f480a156e51276a
cyblogerz <pranavajay2002@gmail.com>
2023-09-28 13:20:05 +0530

    Created repo card

---
+++ src/components/UI/RepoCard.jsx
@@ -0,0 +1,85 @@
+/* eslint-disable react/prop-types */
+import { GoStar, GoRepoForked } from 'react-icons/go';
+
+
+
+
+function RepoCard({repo}) {
+  return (
+    <div className="shadow-sm card bg-2023-void-2 ring-1 ring-2023-manga-3">
+      <div className="relative card-body">
+        <div className="avatar">
+          <div className="w-10 h-10 mb-2 rounded-full">
+            <a
+              href={repo.owner.html_url}
+              title={repo.owner.login}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={repo.owner.avatar_url}
+                width={50}
+                height={50}
+                alt={repo.owner.login}
+                className="rounded-xl"
+              />
+            </a>
+          </div>
+        </div>
+
+        <a
+          href={repo.html_url}
+          title={repo.name}
+          target="_blank"
+          rel="noreferrer"
+          className={`text-3xl card-title link link-hover text-2023-bavarian-gold-2`}
+        >
+          {repo.name}
+        </a>
+
+        <p className="mb-2">{repo.description}</p>
+
+        <div className="card-actions">
+          {repo.topics.map((topic) => (
+            <a
+              key={topic}
+              href={`https://github.com/topics/${topic}`}
+              target="_blank"
+              rel="noreferrer"
+              className={`badge ${
+                topic === 'hacktoberfest'
+                  ? 'bg-2023-bavarian-gold-1 text-2023-void-2'
+                  : 'bg-2023-bavarian-blue-2 text-2023-void-2'
+              }`}
+            >
+              {topic}
+            </a>
+          ))}
+        </div>
+
+        <div className="items-end justify-center h-full card-actions">
+          <div className="shadow stats bg-transparent ring-1 ring-2023-manga-2">
+            <div className="stat">
+              <div className="text-center stat-title items-center inline-flex">
+                <GoStar /> <span className="ml-0.5">Stars</span>
+              </div>
+              <div className="text-center stat-value">
+                {repo.stargazers_count}
+              </div>
+            </div>
+          </div>
+          <div className="shadow stats bg-transparent ring-1 ring-2023-manga-2">
+            <div className="stat">
+              <div className="text-center stat-title items-center inline-flex">
+                <GoRepoForked /> <span className="ml-0.5">Forks</span>
+              </div>
+              <div className="text-center stat-value">{repo.forks}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RepoCard;
--- src/routes/RepoList.jsx
+++ src/routes/RepoList.jsx
@@ -1,42 +1,48 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import  { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import RepoCard from "../components/UI/RepoCard";
 
 const RepoList = () => {
-    const {lang} = useParams();
-    const[repos,setRepos] = useState([]);
-    const [loading,setLoading] = useState(true);
-    const apiUrl = `https://api.github.com/search/repositories?q=topic%3Ahacktoberfest+language%3A${lang}&per_page=21`;
+  const { lang } = useParams();
+  const apiUrl = `https://api.github.com/search/repositories?q=topic%3Ahacktoberfest+language%3A${lang}&per_page=28`;
 
-    useEffect(()=>{
-        fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            setRepos(data.items);
-            setLoading(false)
-        })
-    },[apiUrl])
+  // State to store the fetched data
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Fetch data from the GitHub API
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Set the fetched data in the state and mark loading as false
+        setRepos(data.items || []); // Ensure 'repos' is an array or an empty array if undefined
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, [apiUrl]);
 
-    
   return (
     <div>
-      <h2>Repositories for Language: {lang}</h2>
+      <h2 className="normal-case text-2xl text-center">Repositories for Language: {lang}</h2>
       {loading ? (
         <p>Loading...</p>
       ) : (
         <ul>
-          {repos.map((repo) => (
-            <li key={repo.id}>
-              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                {repo.name}
-              </a>
-            </li>
-          ))}
+          {Array.isArray(repos) && repos.length > 0 ? (
+            repos.map((repo) => (
+              <RepoCard repo={repo} key={repo.id}/>
+            ))
+          ) : (
+            <p>No repositories found.</p>
+          )}
         </ul>
       )}
     </div>
-
   );
-}
+};
 
-export default RepoList
+export default RepoList;
