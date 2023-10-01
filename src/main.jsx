38d62f07da4a366298bc42b6ed22af63586b6149
aakash19here <aakash@Aakashs-MacBook-Air.local>
2023-10-01 21:09:55 +0530

    feat: add react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./routes/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import RepoList from "./routes/RepoList";
+import Blogs from "./routes/Blogs";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Root from './routes/Root';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './routes/Home';
-import RepoList from './routes/RepoList';
-import Blogs from './routes/Blogs';
+const client = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children:[
+    children: [
       {
-      path : '/',
-      element: <Home />
-    },
-    {
-      path:"repos/:lang",
-      element:<RepoList />
-    },
-    {
-      path: "/blogs/",
-    element: <Blogs/>,
-
-    }
-  ]
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "repos/:lang",
+        element: <RepoList />,
+      },
+      {
+        path: "/blogs/",
+        element: <Blogs />,
+      },
+    ],
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
--- src/routes/RepoList.jsx
+++ src/routes/RepoList.jsx
@@ -1,71 +1,62 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import RepoCard from "../components/UI/RepoCard";
 import AnimatedText from "../components/UI/AnimatedText";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
+const fetchRepo = async (apiUrl) => {
+  const response = await axios.get(apiUrl);
+
+  return response.data.items;
+};
 
 const RepoList = () => {
   const { lang } = useParams();
-  const apiUrl = `https://api.github.com/search/repositories?q=topic%3Ahacktoberfest+language%3A${lang}&per_page=21`;
 
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const apiUrl = `https://api.github.com/search/repositories?q=topic%3Ahacktoberfest+language%3A${lang}&per_page=21&page=1`;
 
-  useEffect(() => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["repo"],
+    queryFn: () => fetchRepo(apiUrl),
+  });
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setRepos(data.items || []);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, [apiUrl]);
+  console.log("data hai", data);
 
   return (
     <div className="container mx-auto h-full pt-16">
-    <div className="min-h-screen pt-5">
-
-    <div className="text-center">
-            <div className="w-5/6 max-w-md mx-auto mb-10">
+      <div className="min-h-screen pt-5">
+        <div className="text-center">
+          <div className="w-5/6 max-w-md mx-auto mb-10">
             <h1 className="mb-2 mt-10 text-4xl font-mono">
-            Repositories for : 
+              Repositories for :
             </h1>
             <p className="btn btn-ghost normal-case  font-bold text-5xl text-primary">
               <AnimatedText lang={lang} />
             </p>
-            </div>
-        
-      
-      {loading ? (
-        <p className="text-center">Loading...</p>
-      ) : (
-  
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {Array.isArray(repos) && repos.length > 0 ? (
-            repos.map((repo) => (
-              <RepoCard repo={repo} key={repo.id}/>
-            ))
+          </div>
+
+          {isLoading ? (
+            <p className="text-center">Loading...</p>
           ) : (
-            <p>No repositories found.</p>
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+              {Array.isArray(data) && data.length > 0 ? (
+                data.map((repo) => <RepoCard repo={repo} key={repo.id} />)
+              ) : (
+                <p>No repositories found.</p>
+              )}
+            </div>
           )}
         </div>
-     
-      )}
-      </div>
       </div>
       <div className="w-full flex flex-row gap-4 justify-center items-center">
-      <button className="btn  btn-primary my-10 ">Next Page</button>
-      <Link to='/' className="btn  btn-secondary my-10 ">Go Home</Link>
+        <button className="btn  btn-primary my-10 ">Next Page</button>
+        <Link to="/" className="btn  btn-secondary my-10 ">
+          Go Home
+        </Link>
       </div>
-      
-      </div>
-   
+    </div>
   );
 };
 
 export default RepoList;
-
