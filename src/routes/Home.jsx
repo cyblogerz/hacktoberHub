6e1c0934208c3523c278f0771524a31b95d9bdb2
GouravKumar13 <gouravkumarat@gmail.com>
2023-10-04 02:37:53 +0530

    fix : [BUG] HomeScreen text colour stays white in light mode #21

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,7 @@
 import GradientText from '../components/GradientText';
 import MockupWindow from '../components/UI/MockupWindow';
 import OtherLangButton from '../components/modals/OtherLangButton';
-import {AiOutlineArrowRight} from 'react-icons/ai'
+import { AiOutlineArrowRight } from 'react-icons/ai'
 
 
 
@@ -12,29 +12,29 @@ const Home = () => {
 
   return (
     <div className='container mx-auto w-full h-full '>
-  
-    <div className="flex  flex-row items-center justify-center min-h-screen  gap-10 ">
-    
-    <div className="  w-[80%]">
-    <div>
-    <a href="https://hacktoberfest.com"className="badge badge-neutral rounded-4xl font-mono">hacktoberfest.com</a>
-    <p className="text-6xl text-left font-bold text-white">
-    Find your perfect <br/>
-    <GradientText text={'Hacktoberfest Project'}/>  <br />
-    and contribute today
-    </p>
-    </div>
-    
-       
-   
-        <p className="mt-4 text-lg w-[95%]  ">Discover exciting repositories to contribute to in this year&apos;s<span className="underline "> Hacktoberfest! </span> </p>
-        <div className="flex flex-row mt-16 gap-4 w-[80%] ">
-        <OtherLangButton /><button className="btn  btn normal-case text-xl w-1/2">Resources <AiOutlineArrowRight /></button></div>
-</div>
-<MockupWindow />
+
+      <div className="flex  flex-row items-center justify-center min-h-screen  gap-10 ">
+
+        <div className="  w-[80%]">
+          <div>
+            <a href="https://hacktoberfest.com" className="badge badge-neutral rounded-4xl font-mono">hacktoberfest.com</a>
+            <p className="text-6xl text-left font-bold dark:text-[white] light:">
+              Find your perfect <br />
+              <GradientText text={ 'Hacktoberfest Project' } />  <br />
+              and contribute today
+            </p>
+          </div>
 
 
-    </div>
+
+          <p className="mt-4 text-lg w-[95%]  ">Discover exciting repositories to contribute to in this year&apos;s<span className="underline "> Hacktoberfest! </span> </p>
+          <div className="flex flex-row mt-16 gap-4 w-[80%] ">
+            <OtherLangButton /><button className="btn  btn normal-case text-xl w-1/2">Resources <AiOutlineArrowRight /></button></div>
+        </div>
+        <MockupWindow />
+
+
+      </div>
     </div>
   )
 }
