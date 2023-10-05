5ab1d650b5d7cf5b1e1bfe9b25cfde8059738a3c
GouravKumar13 <gouravkumarat@gmail.com>
2023-10-05 17:54:57 +0530

    header not responsive in mobile view #30

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,20 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/prop-types */
+import MenuIcon from '@mui/icons-material/Menu';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { useClickAway } from "@uidotdev/usehooks";
+import React from "react";
 import { Link } from "react-router-dom";
 const NavBar = (props) => {
+  const [navOpen, setNavOpen] = React.useState(false)
+  const [themOpen, setthemOpen] = React.useState(false)
+
+  function navOpenHandler () {
+    setNavOpen(!navOpen)
+    setthemOpen(false)
+  }
+
   const clickHandler = (e) => {
     const { value: val, name } = e.target;
     props.onThemeChange(val, name);
@@ -18,53 +30,94 @@ const NavBar = (props) => {
   });
 
   return (
-    <div className="navbar bg-base-100 fixed shadow-md p-1 z-10 ">
-      <div className="flex-1">
+    <>
+      <div className="navbar bg-base-100 justify-between fixed shadow-md p-1 z-10 ">
+
+
+
         <Link for="/" className="btn btn-ghost normal-case text-xl">
           <h1 className="">
-
             Hacktober
             <span className="text-accent">Hub</span>
           </h1>
 
         </Link>
-      </div>
 
-      <div className="menu menu-horizontal  ">
-        <Link to="/blogs/" className="btn  normal-case btn-ghost">
-          Blogs
-        </Link>
-        <a
-          className="btn  normal-case btn-ghost"
-          href={ "https://github.com/cyblogerz/hacktoberHub" }
-          target="_blank"
-        >
-          Contribute
-        </a>
-        <li>
-          <details id="detailElem" ref={ ref }>
-            <summary>Themes</summary>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <button className="" onClick={ clickHandler } name="dark">
-                  Dark
-                </button>
-              </li>
-              <li>
-                <button className="" onClick={ clickHandler } name="light">
-                  Light
-                </button>
-              </li>
-              <li>
-                <button className="" onClick={ clickHandler } name="cupcake">
-                  Cupcake
-                </button>
-              </li>
-            </ul>
-          </details>
-        </li>
+
+        <div className="menu md:menu-horizontal hidden">
+          <Link to="/blogs/" className="btn  normal-case btn-ghost">
+            Blogs
+          </Link>
+          <a
+            className="btn  normal-case btn-ghost"
+            href={ "https://github.com/cyblogerz/hacktoberHub" }
+            target="_blank"
+          >
+            Contribute
+          </a>
+          <li>
+            <details id="detailElem" ref={ ref }>
+              <summary >Themes</summary>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <button className="" onClick={ clickHandler } name="dark">
+                    Dark
+                  </button>
+                </li>
+                <li>
+                  <button className="" onClick={ clickHandler } name="light">
+                    Light
+                  </button>
+                </li>
+                <li>
+                  <button className="" onClick={ clickHandler } name="cupcake">
+                    Cupcake
+                  </button>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </div>
+        <div className="md:hidden ">
+          <div className="md:hidden mr-5  relative" onClick={ navOpenHandler }><MenuIcon /></div>
+          { navOpen &&
+            <div className="absolute left-[71%] top-16 flex flex-col translate-x-10 transition-transform">
+              <Link to="/blogs/" className=" font-semibold normal-case btn-ghost">
+                Blogs
+              </Link>
+              <a
+                className="font-semibold normal-case btn-ghost"
+                href={ "https://github.com/cyblogerz/hacktoberHub" }
+                target="_blank"
+              >
+                Contribute
+              </a>
+
+              <details open={ themOpen }>
+                <summary className='font-semibold list-none' onClick={ () => setthemOpen(!themOpen) }  >Themes { themOpen ? <ArrowDropDownIcon /> : <ArrowDropUpIcon /> } </summary>
+                <ul className=" bg-slate-200">
+                  <li>
+                    <button className="" onClick={ clickHandler } name="dark">
+                      Dark
+                    </button>
+                  </li>
+                  <li>
+                    <button className="" onClick={ clickHandler } name="light">
+                      Light
+                    </button>
+                  </li>
+                  <li>
+                    <button className="" onClick={ clickHandler } name="cupcake">
+                      Cupcake
+                    </button>
+                  </li>
+                </ul>
+              </details>
+            </div>
+          }
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
