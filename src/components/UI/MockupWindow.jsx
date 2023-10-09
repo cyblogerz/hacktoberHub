fb4aa6729d0c4ac9f7eba3c25df572530cc18924
cyblogerz <pranavajay2002@gmail.com>
2023-10-09 11:39:08 +0530

    making responsive

--- src/components/UI/MockupWindow.jsx
+++ src/components/UI/MockupWindow.jsx
@@ -2,14 +2,14 @@ import WindowChild from '../WindowChild';
 
 const MockupWindow = () => {
     return (
-        <div className=" px-10 ">
+        <div className="px-10 ">
             
-            <div className="h-[60vh] w-[50vw] flex">
+            <div className="h-[60vh] md:w-[50vw] w-screen mt-10 md:mt-0 mx-10">
                 <div className="mockup-browser  bg-base-300">
                     <div className="mockup-browser-toolbar">
                         <div className="input">https://hacktoberfest.com</div>
                     </div>
-                    <div className="flex flex-col  h-full justify-center items-center px-4 py-16 bg-base-200">
+                    <div className="flex flex-col  h-full justify-center items-center px-4 py-16 ">
                         <WindowChild />
                     </div>
                 </div>
